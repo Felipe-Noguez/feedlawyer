50bc3e24fab90e77de190538827214c7f40bad13
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-01-20 20:55:58 -0300

    Adicioinando atributos da classe CargoEntity

--- src/main/java/br/com/noguezbrothers/feedlawyer/entities/CargoEntity.java
+++ src/main/java/br/com/noguezbrothers/feedlawyer/entities/CargoEntity.java
@@ -1,4 +1,26 @@
 package br.com.noguezbrothers.feedlawyer.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "CARGO")
 public class CargoEntity {
+
+    @Id
+    @Column(name = "id_perfil")
+    private Integer idPerfil;
+
+    @Column(name = "nome")
+    private String nome;
+    
 }
