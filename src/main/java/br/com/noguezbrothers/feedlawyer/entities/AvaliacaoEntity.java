f02d1bd6359b02fa2abcd2e9da9b9ee9bae0ecf1
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-01-20 12:24:03 -0300

    Adicionado atributos da classe AvaliacaoEntity

--- src/main/java/br/com/noguezbrothers/feedlawyer/entities/AvaliacaoEntity.java
+++ src/main/java/br/com/noguezbrothers/feedlawyer/entities/AvaliacaoEntity.java
@@ -1,4 +1,41 @@
 package br.com.noguezbrothers.feedlawyer.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "AVALIACAO")
 public class AvaliacaoEntity {
+
+    @Id
+    @Column(name = "id_avaliacao")
+    private Integer idAvaliacao;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "nome_advogado")
+    private String nomeAdvogado;
+
+    @Column(name = "nota_avaliacao")
+    private Double notaAvaliacao;
+
+    @Column(name = "nome_cliente")
+    private String nomeCliente;
+
+    @Column(name = "sugestao")
+    private String sugestao;
+
+    @Column(name = "email_cliente")
+    private String emailCliente;
 }
