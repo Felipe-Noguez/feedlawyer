b1ab4cc3602963bd69596b6401683e51f5052bd1
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-01-20 21:22:05 -0300

    Adicioinando atributos da classe FuncionarioCargoEntity

--- src/main/java/br/com/noguezbrothers/feedlawyer/entities/pk/FuncionarioCargoPK.java
+++ src/main/java/br/com/noguezbrothers/feedlawyer/entities/pk/FuncionarioCargoPK.java
@@ -1,4 +1,28 @@
 package br.com.noguezbrothers.feedlawyer.entities.pk;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Embeddable
+@Entity(name = "FUNCIONARIO_CARGO")
 public class FuncionarioCargoPK {
+
+    @Id
+    @Column(name = "id_funcionario_cargo")
+    private String idFuncionarioCargo;
+
+    @Column(name = "id_cargo")
+    private Integer idCargo;
+
+    @Column(name = "id_funcionario")
+    private Integer idFuncionario;
+
 }
