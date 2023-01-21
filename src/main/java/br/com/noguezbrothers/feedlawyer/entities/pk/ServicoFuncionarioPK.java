21b3e9f21c761d03765828200956d220ad41ce05
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-01-20 21:28:03 -0300

    Adicionando atributos da classe ServicoFuncionarioEntity

--- src/main/java/br/com/noguezbrothers/feedlawyer/entities/pk/ServicoFuncionarioPK.java
+++ src/main/java/br/com/noguezbrothers/feedlawyer/entities/pk/ServicoFuncionarioPK.java
@@ -1,4 +1,30 @@
 package br.com.noguezbrothers.feedlawyer.entities.pk;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Embeddable
+@Entity(name = "SERVICO_FUNCIONARIO")
 public class ServicoFuncionarioPK {
+
+    @Id
+    @Column(name = "id_servico_funcionario")
+    private Integer idServicoFucionario;
+
+    @Column(name = "id_servico")
+    private Integer idServico;
+
+    @Column(name = "id_funcionario")
+    private Integer idFuncionario;
 }
