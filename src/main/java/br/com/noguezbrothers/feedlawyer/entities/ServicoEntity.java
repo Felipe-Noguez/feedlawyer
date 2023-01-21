7bd89eee1c8d76db34a29abc0b3bfcb7e56a07c7
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-01-20 21:10:38 -0300

    Adicioinando atributos da classe ServicoEntity

--- src/main/java/br/com/noguezbrothers/feedlawyer/entities/ServicoEntity.java
+++ src/main/java/br/com/noguezbrothers/feedlawyer/entities/ServicoEntity.java
@@ -1,4 +1,26 @@
 package br.com.noguezbrothers.feedlawyer.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "SERVICO")
 public class ServicoEntity {
+
+    @Id
+    @Column(name = "id_servico")
+    private Integer idServico;
+
+    @Column(name = "descricao")
+    private String descricao;
+
 }
