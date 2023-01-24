21a9f114c2fe008db2927c3f72496b31cc940bf1
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-01-24 16:27:25 -0300

    Adicionando exceções

---
+++ src/main/java/br/com/noguezbrothers/feedlawyer/exceptions/RegraDeNegocioException.java
@@ -0,0 +1,8 @@
+package br.com.noguezbrothers.feedlawyer.exceptions;
+
+public class RegraDeNegocioException extends Exception {
+
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
