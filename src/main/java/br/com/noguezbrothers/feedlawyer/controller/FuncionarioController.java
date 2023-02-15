75921e10f9d47e4e34b2abbf2f9d5aa33ba94921
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-02-15 19:49:05 -0300

    Alteração em na passagem de parametros em listarFuncionarios e correção do endpoint remover

--- src/main/java/br/com/noguezbrothers/feedlawyer/controller/FuncionarioController.java
+++ src/main/java/br/com/noguezbrothers/feedlawyer/controller/FuncionarioController.java
@@ -1,9 +1,12 @@
 package br.com.noguezbrothers.feedlawyer.controller;
 
-import br.com.noguezbrothers.feedlawyer.dto.paginacaodto.PageDTO;
 import br.com.noguezbrothers.feedlawyer.dto.funcionario.FuncionarioCreateDTO;
 import br.com.noguezbrothers.feedlawyer.dto.funcionario.FuncionarioDTO;
+import br.com.noguezbrothers.feedlawyer.dto.logindto.LoginDTO;
+import br.com.noguezbrothers.feedlawyer.dto.paginacaodto.PageDTO;
+import br.com.noguezbrothers.feedlawyer.enums.Situacao;
 import br.com.noguezbrothers.feedlawyer.exceptions.RegraDeNegocioException;
+import br.com.noguezbrothers.feedlawyer.service.AuthService;
 import br.com.noguezbrothers.feedlawyer.service.FuncionarioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,21 +25,23 @@ import javax.validation.Valid;
 public class FuncionarioController {
 
     private final FuncionarioService funcionarioService;
+    private final AuthService authService;
 
-    @PostMapping("/cadastro")
+    @PostMapping("/cadastrar")
     public ResponseEntity<FuncionarioDTO> cadastrarFuncionario(@Valid @RequestBody FuncionarioCreateDTO funcionarioCreateDTO) throws RegraDeNegocioException {
         return new ResponseEntity<>(funcionarioService.cadastrarFuncionario(funcionarioCreateDTO), HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @GetMapping("/listar")
     public ResponseEntity<PageDTO<FuncionarioDTO>> listarFuncionarios(@RequestParam(required = false) String nome,
-                                                                     @RequestParam(required = false) String cpf,
-                                                                     @RequestParam(required = false)String especializacao,
-                                                                     @RequestParam(required = false)Integer tipoPerfil,
-                                                                     @RequestParam(required = false)Integer idFuncionario,
-                                                                     Integer page,
-                                                                     Integer size) throws RegraDeNegocioException {
-        return new ResponseEntity<>(funcionarioService.listarFuncionarios(nome, cpf, especializacao, tipoPerfil, idFuncionario, page, size), HttpStatus.OK);
+                                                                      @RequestParam(required = false) String cpf,
+                                                                      @RequestParam(required = false) String especializacao,
+                                                                      @RequestParam(required = false) Integer idFuncionario,
+                                                                      @RequestParam(required = false) Integer cargo,
+                                                                      @RequestParam(required = false) Situacao situacao,
+                                                                      @RequestParam(defaultValue = "0") Integer page,
+                                                                      @RequestParam(defaultValue = "10") Integer size) throws RegraDeNegocioException {
+        return new ResponseEntity<>(funcionarioService.listarFuncionarios(nome, cpf, especializacao, idFuncionario, cargo, situacao, page, size), HttpStatus.OK);
     }
 
     @PutMapping("/atualizar/{cpf}")
@@ -44,7 +49,7 @@ public class FuncionarioController {
         return  new ResponseEntity<>(funcionarioService.atualizarFuncionario(funcionarioCreateDTO), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{cpf}")
+    @DeleteMapping("/remover/{cpf}")
     public ResponseEntity<Void> removerFuncionario(@PathVariable("cpf") String cpf) throws RegraDeNegocioException {
         funcionarioService.removerFuncionario(cpf);
         return ResponseEntity.noContent().build();
