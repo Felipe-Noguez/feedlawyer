042cf73bf655510b0ff2af5429d51606aef28f9e
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-02-15 19:44:05 -0300

    Adicionado busca em CargoRepository

--- src/main/java/br/com/noguezbrothers/feedlawyer/repository/CargoRepository.java
+++ src/main/java/br/com/noguezbrothers/feedlawyer/repository/CargoRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CargoRepository extends JpaRepository<CargoEntity, Integer> {
+
+    CargoEntity findAllByNomeContainingIgnoreCase(String nome);
 }
