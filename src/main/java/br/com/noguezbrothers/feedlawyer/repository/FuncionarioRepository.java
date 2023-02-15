b0f44f7488838efabe243837c921149752943603
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-02-15 19:37:58 -0300

    Alteração na busca listarFuncionarios

--- src/main/java/br/com/noguezbrothers/feedlawyer/repository/FuncionarioRepository.java
+++ src/main/java/br/com/noguezbrothers/feedlawyer/repository/FuncionarioRepository.java
@@ -1,6 +1,7 @@
 package br.com.noguezbrothers.feedlawyer.repository;
 
 import br.com.noguezbrothers.feedlawyer.entities.FuncionarioEntity;
+import br.com.noguezbrothers.feedlawyer.enums.Situacao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,18 +13,17 @@ import java.util.Optional;
 @Repository
 public interface FuncionarioRepository extends JpaRepository<FuncionarioEntity, Integer> {
 
-    Page<FuncionarioEntity> findAllByNomeContainingIgnoreCaseOrNomeIsNullAndCpfOrCpfIsNullAndEspecialicazaoContainsIgnoreCaseOrEspecialicazaoIsNullAndTipoPerfilOrTipoPerfilIsNullAndIdFuncionarioOrIdFuncionarioIsNull(String nome, String cpf, String especializacao, Integer tipoPerfil, Integer idFuncionario, PageRequest pageRequest);
-    Page<FuncionarioEntity> findAllByNomeIsNullOrNomeContainingIgnoreCaseAndCpfIsNullOrCpfAndEspecialicazaoIsNullOrEspecialicazaoContainsIgnoreCaseAndTipoPerfilIsNullOrTipoPerfilAndIdFuncionarioIsNullOrIdFuncionario(String nome, String cpf, String especializacao, Integer tipoPerfil, Integer idFuncionario, PageRequest pageRequest);
-
     @Query( "SELECT distinct func " +
             " FROM FUNCIONARIO func " +
+            " inner join func.funcionarioCargoPKS fcar " +
             " WHERE (:nome is null or UPPER(func.nome) LIKE UPPER(concat('%', :nome, '%'))) " +
             " AND (:cpf is null or func.cpf LIKE concat('%', :cpf, '%')) " +
             " AND (:especializacao is null or UPPER(func.especialicazao) LIKE UPPER(concat('%',:especializacao, '%'))) " +
-            " AND (:tipoPerfil is null or func.tipoPerfil = :tipoPerfil)" +
             " AND (:idFuncionario is null or :idFuncionario = func.idFuncionario)" +
+            " AND (:cargo is null or :cargo = fcar.cargoEntity.idPerfil ) " +
+            " AND (:situacao is null or func.situacao = :situacao)" +
             "")
-    Page<FuncionarioEntity> listarFuncionarios(String nome, String cpf, String especializacao, Integer tipoPerfil, Integer idFuncionario, PageRequest pageRequest);
+    Page<FuncionarioEntity> listarFuncionarios(String nome, String cpf, String especializacao, Integer idFuncionario, Integer cargo, Situacao situacao, PageRequest pageRequest);
 
     Optional<FuncionarioEntity> findByCpf(String cpf);
 
