21844fe2b013becaae9978d9968c6e4a676c7a65
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-07-13 23:54:11 -0300

    Feat: Adicionada classe de exceção para Avaliação

---
+++ src/main/java/br/com/noguezbrothers/feedlawyer/exceptions/AvaliacaoException.java
@@ -0,0 +1,8 @@
+package br.com.noguezbrothers.feedlawyer.exceptions;
+
+public class AvaliacaoException extends RegraDeNegocioException{
+
+    public AvaliacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
