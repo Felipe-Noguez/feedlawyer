2fd05ecdcb7cec2ffa1196940e006c920b1a5a34
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-07-21 22:30:17 -0300

    Feat: Métodos para listar paginado, desativar, remover e salvar na Service e Controller

--- src/main/java/br/com/noguezbrothers/feedlawyer/controller/ServicoController.java
+++ src/main/java/br/com/noguezbrothers/feedlawyer/controller/ServicoController.java
@@ -1,7 +1,10 @@
 package br.com.noguezbrothers.feedlawyer.controller;
 
+import br.com.noguezbrothers.feedlawyer.dto.paginacaodto.PageDTO;
 import br.com.noguezbrothers.feedlawyer.dto.servico.ServicoCreateDTO;
 import br.com.noguezbrothers.feedlawyer.dto.servico.ServicoDTO;
+import br.com.noguezbrothers.feedlawyer.dto.servico.ServicoPageDTO;
+import br.com.noguezbrothers.feedlawyer.enums.Situacao;
 import br.com.noguezbrothers.feedlawyer.exceptions.RegraDeNegocioException;
 import br.com.noguezbrothers.feedlawyer.service.ServicoService;
 import lombok.RequiredArgsConstructor;
@@ -9,10 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @Slf4j
@@ -26,5 +26,31 @@ public class ServicoController {
     @PostMapping
     public ResponseEntity<ServicoDTO> cadastrarServico(@RequestBody ServicoCreateDTO servicoCreateDTO) throws RegraDeNegocioException {
         return new ResponseEntity<>(servicoService.cadastrarServico(servicoCreateDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<PageDTO<ServicoPageDTO>> listarAvaliacoes(@RequestParam(required = false) String nomeCliente,
+                                                                    @RequestParam(required = false) Integer idServico,
+                                                                    @RequestParam(required = false) String descricao,
+                                                                    @RequestParam(required = false) String nomeAdvogado,
+                                                                    @RequestParam(required = false) Double notaAvaliacao,
+                                                                    @RequestParam(required = false) Situacao situacao,
+                                                                    @RequestParam(defaultValue = "0") Integer page,
+                                                                    @RequestParam(defaultValue = "10") Integer size) throws RegraDeNegocioException {
+        return new ResponseEntity<>(servicoService.listarServicos(nomeCliente, idServico, descricao, nomeAdvogado, notaAvaliacao, situacao, page, size), HttpStatus.OK);
+    }
+
+    @PutMapping("/{idServico}/desativar")
+    public ResponseEntity<Void> desativarServico(@PathVariable("idServico") Integer idServico) throws RegraDeNegocioException {
+        servicoService.desativarServico(idServico);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{idServico}/remover")
+    public ResponseEntity<Void> removerServico(@PathVariable("idServico") Integer idServico) throws RegraDeNegocioException {
+        servicoService.removerServico(idServico);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/br/com/noguezbrothers/feedlawyer/service/ServicoService.java
+++ src/main/java/br/com/noguezbrothers/feedlawyer/service/ServicoService.java
@@ -1,16 +1,23 @@
 package br.com.noguezbrothers.feedlawyer.service;
 
+import br.com.noguezbrothers.feedlawyer.dto.paginacaodto.PageDTO;
 import br.com.noguezbrothers.feedlawyer.dto.servico.ServicoCreateDTO;
 import br.com.noguezbrothers.feedlawyer.dto.servico.ServicoDTO;
+import br.com.noguezbrothers.feedlawyer.dto.servico.ServicoPageDTO;
 import br.com.noguezbrothers.feedlawyer.entities.ServicoEntity;
 import br.com.noguezbrothers.feedlawyer.entities.UsuarioEntity;
 import br.com.noguezbrothers.feedlawyer.entities.pk.ServicoUsuarioPK;
+import br.com.noguezbrothers.feedlawyer.enums.Situacao;
 import br.com.noguezbrothers.feedlawyer.exceptions.RegraDeNegocioException;
+import br.com.noguezbrothers.feedlawyer.exceptions.ServicoException;
 import br.com.noguezbrothers.feedlawyer.repository.ServicoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @RequiredArgsConstructor
@@ -21,27 +28,84 @@ public class ServicoService {
     private final UsuarioService usuarioService;
 
     public ServicoDTO cadastrarServico(ServicoCreateDTO servicoCreateDTO) throws RegraDeNegocioException {
-        ServicoEntity servicoEntity = new ServicoEntity();
-        ServicoUsuarioPK servicoUsuarioPK = new ServicoUsuarioPK();
-        UsuarioEntity usuarioEntity = usuarioService.buscarPorIdUsuario(usuarioService.getLoggedUser().getIdUsuario());
+        try {
+            ServicoEntity servicoEntity = new ServicoEntity();
+            ServicoUsuarioPK servicoUsuarioPK = new ServicoUsuarioPK();
 
-        servicoEntity.setDescricao(servicoCreateDTO.getDescricao());
+            UsuarioEntity usuarioEntityFuncionario = usuarioService.buscarPorIdUsuario(usuarioService.getLoggedUser().getIdUsuario());
+            UsuarioEntity usuarioEntityCliente = usuarioService.buscarPorIdUsuario(servicoCreateDTO.getIdCliente());
 
-        servicoUsuarioPK.setServicoUsuario(usuarioEntity);
-        servicoUsuarioPK.setServicoEntity(servicoEntity);
+            servicoEntity.setDescricao(servicoCreateDTO.getDescricao());
+            servicoEntity.setSituacao(Situacao.ATIVO);
 
-        Set<ServicoUsuarioPK> usuarios = new HashSet<>();
-        usuarios.add(servicoUsuarioPK);
-        servicoEntity.setServicoUsuarioPKS(usuarios);
+            servicoUsuarioPK.setUsuarioEntityFuncionario(usuarioEntityFuncionario);
+            servicoUsuarioPK.setUsuarioEntityCliente(usuarioEntityCliente);
+            servicoUsuarioPK.setServicoEntity(servicoEntity);
 
-        servicoRepository.save(servicoEntity);
+            Set<ServicoUsuarioPK> usuariosServico = new HashSet<>();
+            usuariosServico.add(servicoUsuarioPK);
+            servicoEntity.setServicoUsuarioPKS(usuariosServico);
 
-        ServicoDTO servicoDTO = new ServicoDTO(servicoEntity);
-        return new ServicoDTO(servicoEntity);
+            servicoRepository.save(servicoEntity);
+
+            return new ServicoDTO(servicoEntity);
+        }catch (RegraDeNegocioException ex){
+            throw new ServicoException("Erro ao cadastrar Serviço");
+        }
     }
 
-    public ServicoEntity getServicoEntityById(Integer idServico) throws RegraDeNegocioException {
+    public PageDTO<ServicoPageDTO> listarServicos(String nomeCliente, Integer idServico, String descricao, String nomeAdvogado, Double notaAvaliacao, Situacao situacao, Integer page, Integer size) throws RegraDeNegocioException {
+        if (page < 0 || size < 0) {
+            throw new RegraDeNegocioException("Tamanho da página ou de elementos não podem ser menor que 0.");
+        }
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<ServicoEntity> servicos = servicoRepository.listarServicos(nomeCliente, idServico, descricao, nomeAdvogado, notaAvaliacao, situacao, pageRequest);
+
+        List<ServicoPageDTO> servicosDTO = servicos.getContent()
+                .stream()
+                .map(servicoEntity -> {
+                    try {
+                        return new ServicoPageDTO(servicoEntity);
+                    } catch (RegraDeNegocioException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .toList();
+
+        if (servicosDTO.isEmpty()) {
+            throw new RegraDeNegocioException("Dados não encontrados.");
+        }
+
+        return new PageDTO<>(servicos.getTotalElements(),
+                servicos.getTotalPages(),
+                page,
+                size,
+                servicosDTO);
+    }
+
+    public void desativarServico(Integer idServico) throws RegraDeNegocioException {
+        ServicoEntity servico = buscarServicoEntityById(idServico);
+
+        if(servico.getSituacao().equals(Situacao.INATIVO)) {
+            throw new RegraDeNegocioException("Serviço já desativado");
+        }
+
+        servico.setSituacao(Situacao.INATIVO);
+
+        servicoRepository.save(servico);
+    }
+
+    public void removerServico(Integer idServico) throws RegraDeNegocioException {
+        servicoRepository.delete(buscarServicoEntityById(idServico));
+    }
+
+    public ServicoEntity buscarServicoEntityById(Integer idServico) throws RegraDeNegocioException {
         return servicoRepository.findById(idServico)
                 .orElseThrow(() -> new RegraDeNegocioException("Serviço não encontrado"));
+    }
+
+    public void save(ServicoEntity servicoEntity) {
+        servicoRepository.save(servicoEntity);
     }
 }
