0756a3aa17d7083ad5d0310d640943b9ef90d6b2
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-07-21 22:19:31 -0300

    Feat: Nova classe de exceção para Servico

---
+++ src/main/java/br/com/noguezbrothers/feedlawyer/exceptions/ServicoException.java
@@ -0,0 +1,8 @@
+package br.com.noguezbrothers.feedlawyer.exceptions;
+
+public class ServicoException extends RegraDeNegocioException{
+
+    public ServicoException(String mensagem) {
+        super(mensagem);
+    }
+}
